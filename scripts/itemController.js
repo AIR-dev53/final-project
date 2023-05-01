369e6c482d791f13d101cbf9aa80cb3282f2c419
romeo8221 <126723192+romeo8221@users.noreply.github.com>
2023-05-01 15:00:46 -0400

    items for our product and local storaging

---
+++ scripts/itemController.js
@@ -0,0 +1,27 @@
+class itemController {
+    constructor() {
+      this.items = [];
+      this.currentId = 0; // initialize the latest id to 0
+    }
+  
+    addItem(name, description, img, createdAt) {
+      const newItem = {
+        id: this.currentId + 1, // increment the latest id and assign it to the new item
+        name,
+        description,
+        img,
+        createdAt
+      };
+      this.items.push(newItem);
+      this.currentId++; // increment the latest id for the next item
+    }
+  }
+const myController = new itemController();
+myController.addItem('Red Velvet Cake', 'Freshly made velvet cake', 'food img');
+myController.addItem('Churro', 'Freshly made churro', 'some img of churro');
+console.log(myController.items);
+
+
+    // Save the items to local storage
+    localStorage.setItem('items', JSON.stringify(itemController.items));
+  
